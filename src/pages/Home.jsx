fc2e4fe8d20c74604fce9dadc5fb66a84f7357ae
kimsoyoung <sy_busay@naver.com>
2024-11-30 01:28:48 +0900

    Home에서 nav 이미지 삽입

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
+import Nav_logo from '../assets/nav_panda_logo_img.png';
+import Nav_user from '../assets/nav_user_img.png';
+
 const Home = () => {
   return (
     <div className="Home">
       <div className="nav_logo">
-        <img />
+        <img src={Nav_logo} />
       </div>
       <div className="nav_center">
         <div>자유게시판</div>
         <div>중고마켓</div>
       </div>
       <div className="nav_user">
-        <img />
+        <img src={Nav_user} />
       </div>
     </div>
   );
