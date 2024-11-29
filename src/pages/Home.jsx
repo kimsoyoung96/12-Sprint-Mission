7701890c6e13820acf19d95b98753f07fbd48718
kimsoyoung <sy_busay@naver.com>
2024-11-30 01:22:14 +0900

    Home에서 nav 틀 잡기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <div className="nav_logo">
+        <img />
+      </div>
+      <div className="nav_center">
+        <div>자유게시판</div>
+        <div>중고마켓</div>
+      </div>
+      <div className="nav_user">
+        <img />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
