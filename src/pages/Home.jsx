96e44a93da1f8d04312451dbf3b07a904858aafc
kimsoyoung <sy_busay@naver.com>
2024-11-30 04:00:33 +0900

    Home에서 사용할 Nav컴포넌트 생성 및 스타일 적용 + Buttonn 사이 gap 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,21 @@ import './Home.css';
 import Nav_logo from '../assets/nav_panda_logo_img.png';
 import Nav_user from '../assets/nav_user_img.png';
 import Button from '../components/Button';
+import Nav from '../components/Nav';
 
 const Home = () => {
   return (
     <div className="Home">
-      <div className="nav_logo">
-        <img src={Nav_logo} />
-      </div>
-      <div className="nav_center">
-        <Button text={'자유게시판'} />
-        <Button text={'중고마켓'} />
-      </div>
-      <div className="nav_user">
-        <img src={Nav_user} />
-      </div>
+      <Nav
+        leftChild={<img src={Nav_logo} />}
+        center={
+          <div>
+            <Button text={'자유게시판'} />
+            <Button text={'중고마켓'} />
+          </div>
+        }
+        rightChild={<img src={Nav_user} />}
+      />
     </div>
   );
 };
