ca0b2bddc937f01dfe198b7f13307d5d8187a706
kimsoyoung <sy_busay@naver.com>
2024-12-01 13:55:54 +0900

    Additem페이지 레이아웃 설정 및 Button 스타일 추가하고 수정하기

--- src/pages/Additem.jsx
+++ src/pages/Additem.jsx
@@ -1,5 +1,34 @@
+import './Additem.css';
+import Nav from '../components/Nav';
+import Nav_logo from '../assets/nav_panda_logo_img.png';
+import Nav_user from '../assets/nav_user_img.png';
+import Button from '../components/Button';
+import Itemtitle from '../components/Itemtitle';
+
 const Additem = () => {
-  return <div>Additem</div>;
+  return (
+    <div className="Additem">
+      <Nav
+        leftChild={<img src={Nav_logo} />}
+        center={
+          <div className="nav_button">
+            <Button text={'자유게시판'} />
+            <Button text={'중고마켓'} type={'nav_activate'} />
+          </div>
+        }
+        rightChild={<img src={Nav_user} />}
+      />
+      {/* 아래는 상품등록 페이지 구현 */}
+      <div className="edit">
+        <div className="edit_title">
+          <Itemtitle
+            leftChild={'상품 등록하기'}
+            rightChild={<Button text={'등록'} type={'register'} />}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Additem;
