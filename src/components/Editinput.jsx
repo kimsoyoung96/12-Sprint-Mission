4857512fc8de949d96f700c879fe8830afb6ac88
kimsoyoung <sy_busay@naver.com>
2024-12-03 03:03:37 +0900

    상품등록페이지에서 이미지 미리보기 동작 추가

--- src/components/Editinput.jsx
+++ src/components/Editinput.jsx
@@ -1,15 +1,39 @@
 import './Editinput.css';
 import img_up from '../assets/img_up.png';
+import { useRef, useState } from 'react';
 
 const Editinput = ({ type, info, placeholder }) => {
+  const [imgFile, setImgFile] = useState('');
+  const imgRef = useRef();
+
+  const saveImgFile = () => {
+    const file = imgRef.current.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImgFile(reader.result);
+    };
+  };
+
   if (type === 'img') {
     return (
-      <div className="image-upload">
-        <label className="upload_input" for="file-input">
-          <img src={img_up} />
-          이미지 등록
-        </label>
-        <input id="file-input" className="hidden_input" type="file" />
+      <div className="imameSection">
+        <div className="image-upload">
+          <label className="upload_input" for="file-input">
+            <img src={img_up} />
+            이미지 등록
+          </label>
+          <input
+            id="file-input"
+            className="hidden_input"
+            type="file"
+            onChange={saveImgFile}
+            ref={imgRef}
+          />
+        </div>
+        <div>
+          <img src={imgFile ? imgFile : ''} />
+        </div>
       </div>
     );
   } else {
