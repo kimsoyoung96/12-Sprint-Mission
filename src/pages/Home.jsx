6ca81a9fa33f50ed4f7bbbd635dd737b44a1a04e
kimsoyoung <sy_busay@naver.com>
2024-12-09 01:53:04 +0900

    Nav 따로 컴포넌트에서 작업(Home컴포넌트에 import가 많고, 중고마켓에 링크를 매번 적용시키기 위함)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import "./Home.css";
-import Nav_logo from "../assets/nav_panda_logo_img.png";
-import Nav_user from "../assets/nav_user_img.png";
 import Button from "../components/Button";
 import Nav from "../components/Nav";
 import Bestitem from "../components/Bestitem";
@@ -33,16 +31,7 @@ const Home = () => {
 
   return (
     <div className="Home">
-      <Nav
-        leftChild={<img src={Nav_logo} />}
-        center={
-          <div className="nav_button">
-            <Button text={"자유게시판"} />
-            <Button text={"중고마켓"} type={"nav_activate"} />
-          </div>
-        }
-        rightChild={<img src={Nav_user} />}
-      />
+      <Nav />
       {/* 아래는 상품 */}
       <div className="item">
         <div className="item_title">
